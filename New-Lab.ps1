65987814ddac0efc5822e4797656a6a9d4805e60
Logan Rakai <lmrakai@gmail.com>
2018-11-24 20:19:39 -0700

    Use latest recommended login command

--- New-Lab.ps1
+++ New-Lab.ps1
@@ -1,4 +1,4 @@
 . .\Variables.ps1
-Login-AzureRmAccount
+Connect-AzureRmAccount
 New-AzureRmResourceGroup -Name $Lab -Location $Region
 New-AzureRmResourceGroupDeployment -Name lab-resources -ResourceGroupName $Lab -TemplateFile .\infrastructure\arm-template.json
