b0496791090e26fc24bd1429ae55abe955c54a86
Logan Rakai <lmrakai@gmail.com>
2019-01-10 12:19:01 -0700

    Reorder Connect-AzureRmAccount to make it easier to skip

--- New-Lab.ps1
+++ New-Lab.ps1
@@ -1,4 +1,4 @@
-. .\Variables.ps1
 Connect-AzureRmAccount
+. .\Variables.ps1
 New-AzureRmResourceGroup -Name $Lab -Location $Region
 New-AzureRmResourceGroupDeployment -Name lab-resources -ResourceGroupName $Lab -TemplateFile .\infrastructure\arm-template.json
